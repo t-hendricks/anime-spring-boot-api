4303ee3b2f9fffc9457aee48637e0e041b297e06
Trevor H <h.trevs@outlook.com>
2023-04-27 16:20:50 -0500

    Initial setup

---
+++ src/main/java/com/favAnime/demo/MyFavAnimeApplication.java
@@ -0,0 +1,13 @@
+package com.favAnime.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFavAnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFavAnimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/favAnime/demo/MyFavAnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.favAnime.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFavAnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
