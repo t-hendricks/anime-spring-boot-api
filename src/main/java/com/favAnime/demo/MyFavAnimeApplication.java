475a280bf2c294683850c4f0d0189c94f2ec3e31
Trevor H <h.trevs@outlook.com>
2023-04-29 10:16:02 -0500

    Add JPA and PostgreSQL depencies

--- src/main/java/com/favAnime/demo/MyFavAnimeApplication.java
+++ src/main/java/com/favAnime/demo/MyFavAnimeApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyFavAnimeApplication {
 
+	/**
+	 * Configure embedded Tomcat.
+	 * Enable Spring MVC default setup.
+	 * Configure and set up the Jackson for JSON.
+	 *
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(MyFavAnimeApplication.class, args);
 	}
