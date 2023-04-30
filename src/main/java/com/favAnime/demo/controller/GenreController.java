bb233173cb3c70289eb24a5f89dceb0597ff35de
Trevor H <h.trevs@outlook.com>
2023-04-29 19:54:19 -0500

    Add Genre controller

---
+++ src/main/java/com/favAnime/demo/controller/GenreController.java
@@ -0,0 +1,10 @@
+package com.favAnime.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // annotated with @ResponseBody; return object in JSON
+@RequestMapping(path = "/api")
+public class GenreController {
+
+}
