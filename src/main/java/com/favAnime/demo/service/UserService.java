0c9ea86acc28fd7ad848cd04b5dcdbc5096ff3ce
Trevor H <h.trevs@outlook.com>
2023-04-30 16:16:32 -0500

    Update User service

--- src/main/java/com/favAnime/demo/service/UserService.java
+++ src/main/java/com/favAnime/demo/service/UserService.java
@@ -1,9 +1,48 @@
 package com.favAnime.demo.service;
 
+import com.favAnime.demo.exception.InformationExistException;
+import com.favAnime.demo.model.User;
+import com.favAnime.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    @Autowired
+    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    /**
+     * Adds user to the repository.
+     *
+     * @param userObject {Object}
+     * @return User {Object}
+     * @throws InformationExistException
+     */
+    public User createUser(User userObject) {
+        if (!userRepository.existsByEmailAddress(userObject.getEmailAddress())) {
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            return userRepository.save(userObject);
+        } else {
+            throw new InformationExistException("User with email address " + userObject.getEmailAddress() + " already exist ");
+        }
+    }
+
+    /**
+     * Searches for an existing user in the repository.
+     *
+     * @param email {String}
+     * @return User {Object}
+     */
+    public User findUserByEmailAddress(String email) {
+        return userRepository.findUserByEmailAddress(email);
+    }
 }
 
