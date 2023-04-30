19aa9a843b86fbac930e445c306fcde0236590c2
Trevor H <h.trevs@outlook.com>
2023-04-30 13:18:22 -0500

    Add respository for anime model

---
+++ src/main/java/com/favAnime/demo/repository/AnimeRepository.java
@@ -0,0 +1,9 @@
+package com.favAnime.demo.repository;
+
+import com.favAnime.demo.model.Anime;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
+}
