335176def3fd5af518be02c881df8537f9736c8f
Trevor H <h.trevs@outlook.com>
2023-04-30 16:29:54 -0500

    Add files to authorize user details

---
+++ src/main/java/com/favAnime/demo/security/MyUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.favAnime.demo.security;
+
+import com.favAnime.demo.model.User;
+import com.favAnime.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findUserByEmailAddress(email);
+        return new MyUserDetails(user);
+    }
+}
