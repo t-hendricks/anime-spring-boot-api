1a6cf483fd7b845d04622f68a03bbe42402e4ee4
Trevor H <h.trevs@outlook.com>
2023-04-30 17:17:11 -0500

    Add jwt request filter

--- src/main/java/com/favAnime/demo/controller/UserController.java
+++ src/main/java/com/favAnime/demo/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.favAnime.demo.controller;
 
 import com.favAnime.demo.model.User;
+import com.favAnime.demo.model.request.LoginRequest;
 import com.favAnime.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +21,15 @@ public class UserController {
         this.userService = userService;
     }
 
+    // http://localhost:9090/auth/users/register
     @PostMapping("/register")
     public User createUser(@RequestBody User userObject) {
         return userService.createUser(userObject);
+    }
+
+    // http://localhost:9090/auth/users/login
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest){
+        return userService.loginUser(loginRequest);
     }
 }
